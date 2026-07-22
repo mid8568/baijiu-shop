99f6c0bfccd9fe0ab971e50ff7230a6b94c41d58
mid8568 <56178383@qq.com>
2026-07-23 04:36:17 +0800

    Create auth.js

---
+++ js/js/auth.js
@@ -0,0 +1,13 @@
+const SUPABASE_URL =
+"https://ukxxmxnubxjezkwbbxdr.supabase.co";
+
+
+const SUPABASE_KEY =
+"sb_publishable_2IFHfms3ombozpvZCvaeEg_2VZ2z5hJ";
+
+
+const client =
+supabase.createClient(
+SUPABASE_URL,
+SUPABASE_KEY
+);
