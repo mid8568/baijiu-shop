87ea0b7e65c23c3d0f4b5eaee1212d8c4d9b0867
mid8568 <56178383@qq.com>
2026-07-23 05:24:19 +0800

    Update login.js

--- js/login.js
+++ js/login.js
@@ -6,6 +6,7 @@ const SUPABASE_KEY =
 "sb_publishable_2IFHfms3ombozpvZCvaeEg_2VZ2z5hJ";
 
 
+
 const client =
 supabase.createClient(
 SUPABASE_URL,
@@ -15,48 +16,105 @@ SUPABASE_KEY
 
 
 
+
+
 async function login(){
 
 
+
 const email =
-document.getElementById("email").value;
+document.getElementById(
+"email"
+).value.trim();
 
 
 
 const password =
-document.getElementById("password").value;
+document.getElementById(
+"password"
+).value.trim();
 
 
 
 
-const {error}=await client.auth.signInWithPassword({
 
-email,
+if(!email || !password){
 
-password
+
+alert(
+"请输入邮箱和密码"
+);
+
+
+return;
+
+
+}
+
+
+
+
+
+
+
+const {data,error}=await client.auth
+.signInWithPassword({
+
+email:email,
+
+password:password
 
 });
 
 
 
 
+
+
+
 if(error){
 
+
 alert(
+
 "登录失败:"
-+error.message
++
+error.message
+
 );
 
+
 return;
+
 
 }
 
 
 
-alert("登录成功");
 
 
-location.href="admin.html";
+
+
+console.log(
+"登录用户:",
+data.user
+);
+
+
+
+
+
+alert(
+"登录成功"
+);
+
+
+
+
+
+window.location.href =
+"admin.html";
+
 
 
 }
